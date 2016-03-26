e1b91e66443eb1044a7bf6a43c20296b7b26c4ae
alexdecapri <alexdecapri@gmail.com>
2016-03-26 11:17:24 -0600

    models added

--- api/models/listmodel.js
+++ api/models/listmodel.js
@@ -1,1 +1,16 @@
 // Build out your model here:
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var List = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  cards: [{
+    type: String,
+    required, true
+  }]
+})
+
+module.exports = mongoose.model("List", List);
--- api/models/userModel.js
+++ api/models/userModel.js
@@ -1,1 +1,16 @@
 // Build out your model here:
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var User = new Schema({
+  "username": {
+    type: String,
+    required: true
+  },
+  "lists": [{
+    type: Schema.ObjectId,
+    ref: "List"
+  }]
+});
+
+module.exports = mongoose.model("User", User);
